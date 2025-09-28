9f8a652462e1c7ebaa787649bdb3ae922fa329fd
Abhinav Rai <abhinavrai.edu@gmail.com>
2025-09-28 23:37:55 +0530

    url changed

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "https://interview-assistant-enja.onrender.com/api",
+  baseURL: "https://interview-assistant-enja.onrender.com/",
   timeout: 30000
 });
 
