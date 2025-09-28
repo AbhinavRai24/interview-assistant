c38e0b41f5535896789cfb6a899b71a26ce85198
Abhinav <raiabhinav.in@gmail.com>
2025-09-28 12:22:38 +0530

    Update server port configuration from 4000 to 5000

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { parseResume } = require('./parseResume');
 const { callGemini } = require('./aiProxy');
 const { v4: uuidv4 } = require('uuid');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
