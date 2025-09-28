9a4c899bf0bf68ace52023f6e258c4bfcd1be2be
Abhinav Rai <abhinavrai.edu@gmail.com>
2025-09-28 23:42:49 +0530

    url test

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "https://interview-assistant-enja.onrender.com/",
+  baseURL: "http://localhost:4000/api",
   timeout: 30000
 });
 
